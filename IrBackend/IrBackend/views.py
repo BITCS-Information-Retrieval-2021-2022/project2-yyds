733de177285191ec7e90b01a4fb7784f221de26d
piaodazhu <2283079466@qq.com>
2021-11-24 12:50:16 +0800

    update backend
    run test

--- IrBackend/IrBackend/views.py
+++ IrBackend/IrBackend/views.py
@@ -2,9 +2,6 @@ from django.http import HttpResponse
 import json
 import requests
 from requests import status_codes
-def test1_views(request):
-    d1 = {'id': 'uid123','name': 'bob','age': 18,'score': 9.7}   
-    return HttpResponse(json.dumps(d1))
 
 POST_FORM = '''
 <form method='post' action='/search/'>
@@ -16,7 +13,7 @@ def hello_view(request):
     return HttpResponse(POST_FORM)
 
 def search_view(request):
-    url = 'http://101.35.199.231:9200/test/_search?'
+    url = 'http://101.35.199.231:9200/scholardb/_search?'
     headers = {'Content-Type': 'application/json'}
     if request.method == 'GET':
         namekey = request.GET.get('key', '')
@@ -27,12 +24,12 @@ def search_view(request):
     else:
         return HttpResponse(status_codes='404')
 
-    query = dict()
-    query['query'] = {'match': {'name': '%s' % namekey}}
-    query['fields'] = ['birth','name', 'company']
-    query['_source'] = False
-    query['sort'] = [{'birth':'desc'}]
-    payload = json.dumps(query)
+    args = dict()
+    args['query'] = {'match': {'name': '%s' % namekey}}
+    # actully we still need fieldsOfStudy!
+    args['_source'] = {'exclude':['coAuthors', 'papers']}
+    # query['sort'] = [{'paperCount':'desc'}]
+    payload = json.dumps(args)
     r = requests.post(url, headers=headers, data=payload)
     if r.status_code != 200:
             return HttpResponse(status=r.status_code)
@@ -43,12 +40,7 @@ def search_view(request):
     details = re_from_es['hits']['hits']
     rdata['authors'] = list()
     for dt in details:
-        tmp = dt['fields']
-        obj = dict()
-        obj['authorId'] = tmp['birth'][0]
-        obj['name'] = tmp['name'][0]
-        obj['affiliations'] = tmp['company']
-        rdata['authors'].append(obj)
+        rdata['authors'].append(dt['_source'])
     return HttpResponse(json.dumps(rdata), content_type='application/json')
 
 def author_view(request, authorid = ''):
@@ -57,15 +49,18 @@ def author_view(request, authorid = ''):
     if authorid == '':
         return HttpResponse(status = 404)
     
-    url = 'http://101.35.199.231:9200/test/_search?'
+    url = 'http://101.35.199.231:9200/scholardb/_search?'
     headers = {'Content-Type': 'application/json'}
-    query = dict()
-    query['query'] = {'match': {'birth': '%s' % authorid}}
-    payload = json.dumps(query)
+    args = dict()
+    args['query'] = {'match': {'authorId': '%s' % authorid}}
+    payload = json.dumps(args)
     r = requests.post(url, headers=headers, data=payload)
     if r.status_code != 200:
             return HttpResponse(status=r.status_code)
     
     re_from_es = json.loads(r.text)
+    if re_from_es['hits']['total']['value'] != 1:
+        return HttpResponse(status=404)
     rdata = re_from_es['hits']['hits'][0]['_source']
-    return HttpResponse(json.dumps(rdata))
+    rdata['fieldsOfStudy'] = ['Computer Science', 'Edge Computing'] # haven't implement. just a place holder
+    return HttpResponse(json.dumps(rdata), content_type='application/json')
